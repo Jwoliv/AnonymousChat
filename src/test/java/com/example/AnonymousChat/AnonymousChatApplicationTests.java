bd075445504bdcd18472c07bdc6b376ec2b4bdd4
Lishchuk Bohdan <bogdanlishchuk04@gmail.com>
2023-05-12 13:09:26 +0300

    Init commit

---
+++ src/test/java/com/example/AnonymousChat/AnonymousChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnonymousChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
